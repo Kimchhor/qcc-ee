2c1c5b1b06376cb3322dba535622d79a3ba6e90a
kimchhor <chivkimchhor7777@gmail.com>
2024-07-04 13:47:21 +0000

    Initial commit

---
+++ test_frontend.py
@@ -0,0 +1,16 @@
+from qiskit import QuantumCircuit
+from qcc import transpile
+
+qc = QuantumCircuit(2)
+
+qc.h(0)
+qc.cx(0, 1)
+
+measure = qc.measure_all()
+
+qir_string = transpile(qc, allow_array_var=True)
+
+with open('qcl_code.ll', 'w') as f:
+    f.write(str(qir_string))
+    # print("file",f)
+
